c6340c7f89049d31f5849a718381b1d25e87fa9b
Luiza Santiago <lu.sntgo@gmail.com>
2022-02-25 00:19:14 -0300

    add basic server structure

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express, { json } from "express";
+import cors from "cors";
+import router from "./routes/index.js";
+
+const app = express();
+app.use(cors());
+app.use(json());
+
+app.use(router);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is listening on port ${process.env.PORT}`);
+});
