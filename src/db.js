a91f70f2824a6f3cb5c9adf5e118641db346c4a4
Luiza Santiago <lu.sntgo@gmail.com>
2022-02-25 00:31:38 -0300

    chore: adjust code for deploy on heroku

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 
 dotenv.config();
-const mongoClient = new MongoClient(process.env.MONGO_URI);
+const mongoClient = new MongoClient(process.env.MONGO_UR);
 await mongoClient.connect();
 
 const db = mongoClient.db("myList");
